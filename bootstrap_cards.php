0a50167e678412c936ad07dbd5095b3c4b156feb
prabhatdash <prabhatdash0376@gmail.com>
2019-11-25 13:14:17 +0530

    1st_Bootstrap_Update

---
+++ bootstrap_cards.php
@@ -0,0 +1,41 @@
+
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+ 
+ 
+
+ <div class="container">
+  <div class="row">
+       <?php
+        for ($i=1; $i <=10 ; $i++) { 
+        
+       ?>
+
+        <div class="col-12 col-md-4  mt-3 ">
+      <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+    </div>
+        <?php
+
+      }
+        ?>
+        
+   </div>
+</div>
+
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+  <script src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js"></script>
+</body>
